f3f8c1c48af41aae09b6e77553474e85b2e6fdf8
Fernando Mercado <fermercado@live.com>
2024-01-14 19:38:45 -0300

    chore: updade middleware

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -6,13 +6,13 @@ export const authMiddleware = (
   res: Response,
   next: NextFunction,
 ) => {
-  const token = req.headers.authorization;
+  const token = req.headers.authorization?.split(' ')[1];
 
   if (!token) {
     return res.status(401).send({ message: 'Token not provided.' });
   }
 
-  jwt.verify(token, 'process.env.JWT_SECRET', (error, decoded) => {
+  jwt.verify(token, process.env.JWT_SECRET || '', (error, decoded) => {
     if (error) {
       return res.status(401).send({ message: 'Invalid Token.' });
     }
