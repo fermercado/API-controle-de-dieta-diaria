a2ab4edbbcff935762eacc822631caa6989727dc
Fernando Mercado <fermercado@live.com>
2024-01-14 19:36:47 -0300

    feat: added meal in ormconfig

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,12 +1,13 @@
 import { DataSource } from 'typeorm';
 import { User } from './entities/User';
+import { Meal } from './entities/Meal';
 
 const databaseUrl = process.env.DATABASE_URL || 'fallback-database.sqlite';
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
   database: databaseUrl,
-  entities: [User],
+  entities: [User, Meal],
   synchronize: true,
   logging: false,
 });
