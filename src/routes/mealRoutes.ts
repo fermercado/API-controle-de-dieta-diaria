48dcbe8d316e39fcdb24c4f1f2055a1913f1f929
Fernando Mercado <fermercado@live.com>
2024-01-15 17:26:46 -0300

    feat: added mealRoutes

--- src/routes/mealRoutes.ts
+++ src/routes/mealRoutes.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import { CreateMealController } from '../controllers/meal/createMeal';
 import { authMiddleware } from '../middleware/authMiddleware';
+import { UpdateMealController } from '../controllers/meal/UpdateMeal';
 
 const router = express.Router();
 
 router.post('/meals', authMiddleware, (req, res) => {
   const controller = new CreateMealController();
+  controller.handle(req, res);
+});
+
+router.put('/meals/:id', authMiddleware, (req, res) => {
+  const controller = new UpdateMealController();
   controller.handle(req, res);
 });
 
