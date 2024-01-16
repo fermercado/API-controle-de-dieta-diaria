aa5a0350db58ec9014f021e581f8abd01df2f423
Fernando Mercado <fermercado@live.com>
2024-01-16 09:58:15 -0300

    feat: added getMealServices

---
+++ src/services/meal/GetMealService.ts
@@ -0,0 +1,18 @@
+import { Meal } from '../../entities/Meal';
+import { AppDataSource } from '../../ormconfig';
+import { MealRepository } from '../../repositories/MealRepository';
+
+export class GetMealService {
+  private mealRepository: MealRepository;
+
+  constructor() {
+    this.mealRepository = new MealRepository(AppDataSource);
+  }
+
+  async execute(mealId: number, userId: number): Promise<Meal | null> {
+    return await this.mealRepository.GetMeal({
+      id: mealId,
+      user: { id: userId },
+    });
+  }
+}
