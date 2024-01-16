fc98b27f5de0b9de2790a2fb1c80f9de1bce3071
Fernando Mercado <fermercado@live.com>
2024-01-16 15:53:30 -0300

    chore: updgrade UserRepository

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,7 +1,11 @@
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { User } from '../entities/User';
 
 export class UserRepository extends Repository<User> {
+  constructor(dataSource: DataSource) {
+    super(User, dataSource.createEntityManager());
+  }
+
   async findByEmail(email: string): Promise<User | null> {
     return this.findOne({ where: { email } });
   }
