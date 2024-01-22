0cdebe3bdd594043a4dd84efcd6bf7c52a3c21f8
Fernando Mercado <fermercado@live.com>
2024-01-22 11:20:06 -0300

    chore: update erros response update meal

--- src/controllers/meal/UpdateMeal.ts
+++ src/controllers/meal/UpdateMeal.ts
@@ -38,7 +38,8 @@ export class UpdateMealController {
       return response.json(meal);
     } catch (error: any) {
       if (error instanceof z.ZodError) {
-        return response.status(400).json({ errors: error.errors });
+        const errorMessages = error.errors.map((e) => ({ message: e.message }));
+        return response.status(400).json({ errors: errorMessages });
       }
       return response.status(500).json({ error: error.message });
     }
